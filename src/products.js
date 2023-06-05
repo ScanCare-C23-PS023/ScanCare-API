543e107246fe3a4d56f0d23a2d96e1eb79f8d0cc
syauqil <syauqil.amin2002@gmail.com>
2023-06-05 19:53:32 +0700

    products update

--- src/products.js
+++ src/products.js
@@ -1,99 +1,135 @@
 const products = [
     {
         id : "1",
-        brand : "Carnier",
+        brand : "Garnier",
         kind : "Face Wash",
         rating : "4.5",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/Carnier.png",
-        bgColor : "#D1E0D5"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/Garnier.png",
+        bgColor : "#D1E0D5",
+        desc: "A face wash that will gently cleanses your face and helps brighten your skin. It uses natural ingredients and Lemon Extract to provide gentle cleansing and skin brightening. The lemon aroma makes the skin feeling refreshed after use.",
+        time: "Twice a day",
+        ingredients: "Vitamin C"
     },
     {
         id : "2",
-        brand : "L'Oraal",
+        brand : "L'Oreal",
         kind : "Micro Essence",
         rating : "5",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/L'Oraal.png",
-        bgColor : "#D1E0D5"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/L'Oreal.png",
+        bgColor : "#D1E0D5",
+        desc: "L'Oréal Paris Revitalift Crystal Micro Essence is an essence that promotes crystal-clear skin. It is formulated with Micronized Technology to penetrate up to 10 layers of the skin. The lightweight texture absorbs quickly without leaving a sticky residue. It will help you achieve healthy skin with fast skin regeneration to prevent premature aging.",
+        time: "After cleansing face",
+        ingredients: "Centella Asiatica"
     },
     {
         id : "3",
-        brand : "Asnec",
+        brand : "Acnes",
         kind : "Face Wash",
         rating : "4.3",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/Asnec.png",
-        bgColor : "#F6E3E7"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/Acnes.png",
+        bgColor : "#F6E3E7",
+        desc: "This face wash will help you cleanse your face, maintain its moisture, and promote skin brightening. It also helps fighting acne-causing bacteria, preventing acne and fading acne scars while brightening the skin.",
+        time: "Twice a day",
+        ingredients: "Natural Active Ingredient"
     },
     {
         id : "4",
-        brand : "Vardah",
+        brand : "Wardah",
         kind : "Moisturizer",
         rating : "4.8",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/Vardah.png",
-        bgColor : "#F6E3E7"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/Wardah.png",
+        bgColor : "#F6E3E7",
+        desc: "A moisturizer specifically designed for acne-prone skin, providing hydration and protection from UV rays.",
+        time: "In the morning",
+        ingredients: "DermaTreat Actives, Willowherb"
     },
     {
         id : "5",
-        brand : "Scarret",
+        brand : "Scarlett",
         kind : "Acne Serum",
         rating : "4.9",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/Scarret.png",
-        bgColor : "#D1E0D5"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/Scarlett.png",
+        bgColor : "#D1E0D5",
+        desc: "Scarlett Whitening Acne Serum has the ability to eliminate acne and prevent acne from reappearing, while maintaining the skin's health.",
+        time: "Twice a day",
+        ingredients: "Salicylic Acid"
     },
     {
         id : "6",
-        brand : "Eminem",
+        brand : "Emina",
         kind : "Moisturizer",
         rating : "4.6",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/Eminem.png",
-        bgColor : "#D1E0D5"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/Emina.png",
+        bgColor : "#D1E0D5",
+        desc: "This moisturizer contain Summer Plum Extract and Vitamin E that will brighten and moisturize your face. It is also enriched with UVA and UVB filters to protect the skin from the sun. Its lightweight formula can be used daily and gives the face a matte appearance all day long.",
+        time: "Twice a day",
+        ingredients: "Summer Plum Extract"
     },
     {
         id : "7",
-        brand : "MR Glow",
+        brand : "MS Glow",
         kind : "Night Cream",
         rating : "4.8",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/MR%20Glow.png",
-        bgColor : "#D1E0D5"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/MS%20Glow.png",
+        bgColor : "#D1E0D5",
+        desc: "MS Glow Acne Night Cream is a night cream specifically designed for oily and acne-prone skin. It is enriched with Tea Tree Oil and Sulfur as antibacterial agents to control excess oil and care for acne-prone skin, providing soothing, calming, and healing effects for acne on your face.",
+        time: "Before bed",
+        ingredients: "Tea Tree Oil"
     },
     {
         id : "8",
-        brand : "CrosX",
+        brand : "CosrX",
         kind : "Gel Cleanser",
         rating : "4.6",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/CrosX.png",
-        bgColor : "#F6E3E7"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/CosrX.png",
+        bgColor : "#F6E3E7",
+        desc: "COSRX LOW PH GOOD MORNING GEL CLEANSER is a facial cleanser used to soothe, exfoliate, moisturize, and cleanse the skin. Its formulation has a pH level similar to the skin's pH, effectively cleaning the face to make it clean and fresh.",
+        time: "Twice a day",
+        ingredients: "Cocamidopropyl Betaine"
     },
     {
         id : "9",
-        brand : "Pand's",
+        brand : "Pond's",
         kind : "Facial Foam",
         rating : "4.5",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/Pand's.png",
-        bgColor : "#F6E3E7"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/Pond's.png",
+        bgColor : "#F6E3E7",
+        desc: "POND'S ACNE SOLUTION FACIAL FOAM is a facial foam cleanser with Thymo-T Essence, Niacinamide, and Salicylic Acid, which help cleanse the face from dust, pollution, and acne-causing bacteria.",
+        time: "Twice a day",
+        ingredients: "Thymo-T Essence"
     },
     {
         id : "10",
-        brand : "Kohf",
+        brand : "Kahf",
         kind : "Face Wash",
         rating : "4.4",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/Kohf.png",
-        bgColor : "#D1E0D5"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/Kahf.png",
+        bgColor : "#D1E0D5",
+        desc: "The Kahf Oil and Acne Care Face Wash has a unique formula that combines HydroBalanceTM and Pure Cleanse, resulting in a thoroughly clean and deeply moisturized face without make you worry about dry skin or tight skin.",
+        time: "Twice a day",
+        ingredients: "HydroBalance"
     },
     {
         id : "11",
-        brand : "Dermatex",
+        brand : "Dermatix",
         kind : "Gel",
         rating : "4.9",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/Dermatex.png",
-        bgColor : "#F6E3E7"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/Dermatix.png",
+        bgColor : "#F6E3E7",
+        desc: "Dermatix Acne Spot Care contains Cyclopentasiloxane (CPX), which is a quick-drying silicone gel that spreads easily and forms a thin, fast-drying layer.",
+        time: "Twice a day",
+        ingredients: "Niacinamide"
     },
     {
         id : "12",
-        brand : "Saff",
+        brand : "Safi",
         kind : "Face Wash",
         rating : "4.3",
-        imageURL : "https://storage.googleapis.com/scancare-db/products/Saff.png",
-        bgColor : "#F6E3E7"
+        imageURL : "https://storage.googleapis.com/scancare-db/products/Safi.png",
+        bgColor : "#F6E3E7",
+        desc: "Safi White Expert Purifying Cleanser is a face wash that contains natural ingredients such as Habbatus Sauda extract and OxyWhite technology. It effectively cleanse dust and impurities that clog the pores, removes makeup residue thoroughly, helps maintain the skin's moisture, and is suitable for all skin types.",
+        time: "Twice a day",
+        ingredients: "Habbatus Sauda"
     },
 ];
 
